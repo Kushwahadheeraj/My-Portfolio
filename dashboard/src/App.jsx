cfcb609d25fca62d529e7a0c6dbbf82e0354391d
Kushwahadheeraj <kushwahadheeraj245@gmail.com>
2024-07-12 11:42:40 +0530

    dashboard

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
-import HomePage from "./pages/HomePage"
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
 
 const App=()=>{
   return(
     <Router>
       <Routes>
         <Route path="/" element={<HomePage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/password/forgot" element={<ForgotPassword/>}/>
+        <Route path="/password/reset" element={<ResetPassword/>}/>
       </Routes>
     </Router>
   )
